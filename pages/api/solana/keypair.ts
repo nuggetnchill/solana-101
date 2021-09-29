db9e5299714c2e249b36a8db57f4b713fad06975
unknown <nuggetnchill@gmail.com>
2021-09-29 15:45:32 -0500

    finished part 1

--- pages/api/solana/keypair.ts
+++ pages/api/solana/keypair.ts
@@ -1,5 +1,6 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {Keypair} from '@solana/web3.js';
+import {PublicKey} from 'near-api-js/lib/utils';
 
 type ResponseT = {
   secret: string;
@@ -10,8 +11,8 @@ export default function keypair(
   res: NextApiResponse<string | ResponseT>,
 ) {
   try {
-    const keypair = undefined;
-    const address = undefined;
+    const keypair = Keypair.generate();
+    const address = keypair?.publicKey.toString();
     const secret = JSON.stringify(Array.from(keypair.secretKey));
     res.status(200).json({
       secret,
